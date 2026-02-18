dad19382a4d8590b79572be6d4cd8f9db701bda0
DlegendZ <raynaldarvanlim@gmail.com>
2026-02-19 00:39:57 +0800

    upload to local storage and db for url

--- Phase 2 Backend With Python/File upload/app/main.py
+++ Phase 2 Backend With Python/File upload/app/main.py
@@ -4,19 +4,40 @@ from app.database import get_db
 from app.models import Image
 from fastapi.responses import StreamingResponse
 import io
+import os
 
 app = FastAPI()
 
+# @app.post("/upload")
+# async def upload_image(file: UploadFile = File(...), db: Session = Depends(get_db)):
+#     content = await file.read()
+
+#     image = Image(data=content)
+#     db.add(image)
+#     db.commit()
+#     db.refresh(image)
+
+#     return {"id": image.id}
+
+UPLOAD_DIR = "uploads"
+
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    content = await file.read()
 
-    image = Image(data=content)
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+    file_location = f"{UPLOAD_DIR}/{file.filename}"
+
+    with open(file_location, "wb") as f:
+        content = await file.read()
+        f.write(content)
+
+    image = Image(file_path=file_location)
     db.add(image)
     db.commit()
     db.refresh(image)
 
-    return {"id": image.id}
+    return {"file_path": file_location}
 
 @app.get("/images/{image_id}")
 def get_image(image_id: int, db: Session = Depends(get_db)):
--- Phase 2 Backend With Python/File upload/app/models.py
+++ Phase 2 Backend With Python/File upload/app/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, LargeBinary
+from sqlalchemy import Column, String, Integer
 from app.database import Base
 
 class Image(Base):
     __tablename__ = "images"
 
     id = Column(Integer, primary_key=True, index=True)
-    data = Column(LargeBinary, nullable=False)
+    file_path = Column(String, nullable=False)
